8480375c055eeae8edfa0c4c37166ec1c5ab49b9
Athanasios Kefalas <a.kefalas@msensis.com>
2022-04-06 13:19:50 +0300

    VERSION 0.9.1

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Stitcher", // Version: 0.9
+    name: "Stitcher", // Version: 0.9.1
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
